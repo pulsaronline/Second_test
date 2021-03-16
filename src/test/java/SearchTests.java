6f2e32ee66646e13a43c2eb1122de43965348e96
Pulsaronline <beslite@gmail.com>
2021-03-16 21:22:46 +0300

    some fixes

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -2,6 +2,8 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
@@ -21,11 +23,14 @@ public class SearchTests {
         //???
         $(byId("userNumber")).setValue("9263069330").pressEnter();
         $(By.cssSelector("[type='year-month-date']")).setValue("2018-06-01");
-        $(byId("subjectsInput")).setValue("QA engineering study").pressEnter();
+        $(byClassName("subjects-auto-complete__input")).setValue("QA engineering study").pressEnter();
         //$(byId("dateOfBirthInput")).setValue("02/20/2002").pressEnter();
+        $(byClassName("custom-control-input")).click();
 
+        File file = $("#cv").uploadFile(new File("src/test/java/../resources/hello_world.txt"));
+        $("#submit").click();
 
-        $("#search").shouldHave(text("selenide.org"));
+        //$("#search").shouldHave(text("selenide.org"));
         Selenide.sleep(1000);
     }
 }
