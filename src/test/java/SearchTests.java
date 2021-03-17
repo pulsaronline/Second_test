2043a35820165adb8e0b3d3d5f614eb81f793a1b
Pulsaronline <beslite@gmail.com>
2021-03-17 13:57:04 +0300

    some fixes 3

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,5 +1,7 @@
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -12,29 +14,39 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
 
+    @BeforeAll
+    public static void init() {
+        Configuration.browser = "chrome";
+        Configuration.startMaximized = true;
+    }
+
     @Test
     void selenideSearchTest() {
         open("https://demoqa.com/automation-practice-form");
         Selenide.sleep(500);
         $(byId("firstName")).setValue("Jason");
         $(byId("lastName")).setValue("Born");
-        $(byId("userEmail")).setValue("beslite@gmail.com");
+        $(byId("userEmail")).setValue("jborn@gmail.com");
         $(byText("Male")).click();
-        $(byId("userNumber")).setValue("9263069330");
+        $(byId("userNumber")).setValue("1118889991");
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOptionByValue("2");
+        $(".react-datepicker__month-select").selectOptionByValue("4");
         $(".react-datepicker__year-select").selectOptionByValue("1977");
-        $(".react-datepicker__day.react-datepicker__day--010").click();
+        $(".react-datepicker__day.react-datepicker__day--012").click();
         //???
-        $(byId("subjectsInput")).setValue("QA Guru homework").click();
-        $(byText("Sports")).click();
-        File file = $("#uploadPicture").uploadFile(new File("src/test/resources/hello_world.txt"));
-        //$("#uploadPicture").click();
-        $(byId("currentAddress")).setValue("Another world").pressTab();
+        $(byId("subjectsInput")).setValue("QA Guru homework");
+        //$(byId("subjectsContainer")).setValue("QA Guru homework");
 
-       // $("#search").shouldHave(text("beslite@gmail.com"));
-        Selenide.sleep(3000);
-        $("#Submit").click();
-        Selenide.sleep(3000);
+        $(byText("Sports")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/hello_world.txt"));
+        $(byId("currentAddress")).setValue("Mars").pressTab();
+        $(byId("state")).click();
+        $(byText("NCR")).click();
+        $(byId("city")).click();
+        $(byText("Noida")).click();
+        $(byId("submit")).pressEnter();
+        Selenide.sleep(2000);
+
+        // $("#search").shouldHave(text("beslite@gmail.com"));
     }
 }
