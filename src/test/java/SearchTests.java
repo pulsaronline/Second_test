2920aa487817873882e7b926ace3cec700aeb122
Pulsaronline <beslite@gmail.com>
2021-03-17 14:52:39 +0300

    some fixes 4

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -16,37 +16,69 @@ public class SearchTests {
 
     @BeforeAll
     public static void init() {
+        //use Chrome browser, fullscreen
         Configuration.browser = "chrome";
         Configuration.startMaximized = true;
     }
 
     @Test
     void selenideSearchTest() {
+        //work with https://demoqa.com/automation-practice-form
+
         open("https://demoqa.com/automation-practice-form");
+
+        //check the header
+
+        $(".main-header").shouldHave(text("Practice Form"));
+
+        //wait ~0,5 sec
+
         Selenide.sleep(500);
+
+        //type name, surname, email, gender, phone number
+
         $(byId("firstName")).setValue("Jason");
         $(byId("lastName")).setValue("Born");
         $(byId("userEmail")).setValue("jborn@gmail.com");
         $(byText("Male")).click();
         $(byId("userNumber")).setValue("1118889991");
+
+        //type date of birth
+
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue("4");
         $(".react-datepicker__year-select").selectOptionByValue("1977");
         $(".react-datepicker__day.react-datepicker__day--012").click();
-        //???
-        $(byId("subjectsInput")).setValue("QA Guru homework");
-        //$(byId("subjectsContainer")).setValue("QA Guru homework");
+
+        //type subject
+        //$(byId("subjectsInput")).setValue("Subjects");
+        $("#subjectsContainer").click();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#subjectsInput").setValue("English").pressEnter();
+
+        //type hobbie
 
         $(byText("Sports")).click();
+
+        //load file hello_world.txt
+
         $("#uploadPicture").uploadFile(new File("src/test/resources/hello_world.txt"));
-        $(byId("currentAddress")).setValue("Mars").pressTab();
+
+        //type address
+
+        $(byId("currentAddress")).setValue("LA, Oak str., 13").pressTab();
+
+        //type state
+
         $(byId("state")).click();
         $(byText("NCR")).click();
+
+        //type city
         $(byId("city")).click();
         $(byText("Noida")).click();
+
+        //submit the form
         $(byId("submit")).pressEnter();
         Selenide.sleep(2000);
-
-        // $("#search").shouldHave(text("beslite@gmail.com"));
     }
 }
